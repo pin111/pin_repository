300700448e604b712cdb1fb1fe8560a4fca94424
pin111 <143799621+pin111@users.noreply.github.com>
2023-10-12 20:53:57 +0800

    Add files via upload

---
+++ 102101437/PairWork/page/page2/page2.js
@@ -0,0 +1,96 @@
+// pages/page2/page2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  turnp1(){
+    wx.redirectTo({
+      url: '/pages/page1/page1',
+    })
+  },
+  turnp4(){
+    wx.redirectTo({
+      url: '/pages/page4/page4',
+    })
+  },
+  turnp6(){
+    wx.redirectTo({
+      url: '/pages/page6/page6',
+    })
+  },
+  turnp7(){
+    wx.redirectTo({
+      url: '/pages/page7/page7',
+    })
+  },
+  turnp8(){
+    wx.redirectTo({
+      url: '/pages/page8/page8',
+    })
+  },
+  turnp11(){
+    wx.redirectTo({
+      url: '/pages/page11/page11',
+    })
+  },
+
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
